aa881e9fea80b34baa20fa96dec4f5b08c94da83
Mayuresh Teli <mayureshteli25@gmail.com>
2025-07-03 14:02:06 +0530

    Add files via upload

---
+++ GUESS THE NUMBER.py
@@ -0,0 +1,18 @@
+import random
+a=random.randint(1,100)
+i=1
+print("GUESS THE NUMBER BETWEEN 1 TO 100")
+while(i<=7):
+    b=int(input("ENTER THE NUMBER :-"))
+    if(a==b):
+        print("YOU WIN THE NUMBER IS ",a)
+        break
+    elif(b>=a-10 and b<a):
+        print("YOU ARE TOO CLOSE ")
+    elif(b<=a+10 and b>a):
+        print(" YOU ARE TOO CLOSE ")
+    else:
+        print("YOU ARE TOO FAR")
+    i=i+1 
+else:      
+    print("YOU ARE OUT OF CHANCHES THE CORRECT ANSWER IS",a)
